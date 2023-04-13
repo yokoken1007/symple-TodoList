79eabd9369bd7f725afafefc2bddff1f2c7e8260
kyokoyama <yokoyamakenichi@yokoyamakenichinoMacBook-Air.local>
2023-04-13 09:10:07 +0900

    bitrise差分アップロード2

--- symple-todolist/Controllers/FirstViewController.swift
+++ symple-todolist/Controllers/FirstViewController.swift
@@ -28,7 +28,7 @@ class FirstViewController: UIViewController {
         
         setUpViews()
         realmURLCheck()
-        print("bitrise差分テスト")
+        print("bitrise差分テスト2")
         
     }
     
